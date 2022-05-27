6b9d1b2a888a4911020205e4ae164da4373f267c
lommel <monkeytitle@hotmail.com>
2022-05-27 11:59:39 +0700

    add:not-found

--- routes/id.js
+++ routes/id.js
@@ -5,6 +5,9 @@ var db = require('../database/query');
 router.get('/', function(req, res, next) {
   const id = req.query.id
   const data = db.getOne(id);
+  if (data === undefined) {
+    res.status(404).json({ message: 'Not Founds' });
+  }
   res.status(200).json({ data: data });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const db = require('../database/query');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   const data = await db.getAll();
+  console.log(data);
+ 
+  
   res.json({ title: 'tcas65_data', data: data });
+  
 
   
 });
